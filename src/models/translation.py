98776ad6cebdff009bc2485616e6b3e5c97bc6a3
yalobur <yar-lobur@yandex.ru>
2024-08-24 16:26:03 +0300

    implemented google translate api data parser; improved models.

--- src/models/translation.py
+++ src/models/translation.py
@@ -1,15 +1,27 @@
 from pydantic import BaseModel, Field
 
 
+class PartOfSpeechAndSamples(BaseModel):
+    part_of_speech: str
+    samples: list[str] = []
+
+
 class Translation(BaseModel):
     language: str
-    definitions: list[str] = []
-    synonyms: list[str] = []
-    translations: list[str] = []
+    definitions: list[PartOfSpeechAndSamples] = []
+    synonyms: list[PartOfSpeechAndSamples] = []
+    possible_translations: list[PartOfSpeechAndSamples] = []
     examples: list[str] = []
 
 
 class Word(BaseModel):
     word: str = Field(..., alias='_id')
     original_language: str
-    translations: list[Translation]
+    translations: list[Translation] = []
+
+
+__all__ = [
+    'Word',
+    'PartOfSpeechAndSamples',
+    'Translation',
+]
